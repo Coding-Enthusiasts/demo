f86ede63a3ae42babe7fd56c2ad000733d5bf6ba
a641777 <vidya.thakare@atos.net>
2020-09-15 17:42:50 +0530

    demo

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : demo.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!! vidya here" << endl; // prints Hello World!!!
+	return 0;
+}
